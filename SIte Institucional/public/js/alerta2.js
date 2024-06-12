e948b667edf886e0892b27ac89b9f838fa105402
samussouza <samuel.ssouza@sptech.school>
2024-06-11 21:48:56 -0300

    adicionando configurações iniciais para os alertas

---
+++ SIte Institucional/public/js/alerta2.js
@@ -0,0 +1,6 @@
+//variaveis que armazenam as valores atuais das medidas de temp e lumin
+ temperaturaAtual = sessionStorage.MEDIA_ATUALTEMP;
+ luminocidadeAtual = sessionStorage.MEDIA_ATUALLUMIN;
+
+
+console.log('Testando metricas alertas:', temperaturaAtual, luminocidadeAtual);
